379cc004a35495be1ad105f5ec69fbf2b9725f4f
spartakusQ <spartakus1688@gmail.com>
2017-04-05 12:50:14 +0300

    Make to new factory to test "create last comments"

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,4 +1,12 @@
 FactoryGirl.define do
-  factory :article do  
+  factory :article do
+    #создание новой фабрики article_with_comments
+    factory :article_with_comments do
+      #после слздания article
+      after :create do |article, evaluator|
+        #создаём список из 3 комментариев
+        create_list :comment, 3, article: article
+      end
+    end
   end
 end
