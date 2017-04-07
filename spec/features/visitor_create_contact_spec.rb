193ef55afa7980184bd8ebfb96a0268979910306
spartakusQ <spartakus1688@gmail.com>
2017-04-07 18:52:11 +0300

    Update contact test "email & message" click button

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -7,4 +7,10 @@ feature "Contact creation" do
     expect(page).to have_content I18n.t ('contacts.contact_us')
 
   end
+  scenario "allow guest to create contact" do
+    visit '/contacts'
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'lalalala'
+    click_button 'Send message'
+  end
 end
