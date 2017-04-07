476ca47ae82e83ba145ff5717318ce5fd7ecc378
spartakusQ <spartakus1688@gmail.com>
2017-04-07 18:54:52 +0300

    Add commit on test

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -7,10 +7,16 @@ feature "Contact creation" do
     expect(page).to have_content I18n.t ('contacts.contact_us')
 
   end
+  #новый тест для страницы сонтакт и полей: email & message
+
   scenario "allow guest to create contact" do
+    #посетить страницу contacts
     visit '/contacts'
+    #заполнить поле email
     fill_in :contact_email, :with => 'user@example.com'
+    #заполнить поле message
     fill_in :contact_message, :with => 'lalalala'
+    #нажать на кнопку send message
     click_button 'Send message'
   end
 end
