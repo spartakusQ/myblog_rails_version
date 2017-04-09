8460e07fe58c8a811c8961b151486ff6726a4cf5
spartakusQ <spartakus1688@gmail.com>
2017-04-09 19:04:53 +0300

    Create method to test sign_up

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -3,14 +3,18 @@ require "spec_helper"
 feature "Contact creation" do
   scenario "allows guest to contact page" do
     visit new_user_registration_path
-
-    fill_in :user_email, :with => 'user@example.com'
-    fill_in :user_username, :with => 'Bob23'
-    fill_in :user_password, :with => 'lalala@#'
-    fill_in :user_password_confirmation, :with => 'lalala@#'
-
-    click_button 'Sign up'
-
+    sign_up
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
+end
+
+def sign_up
+
+  fill_in :user_email, :with => 'user@example.com'
+  fill_in :user_username, :with => 'Bob23'
+  fill_in :user_password, :with => 'lalala@#'
+  fill_in :user_password_confirmation, :with => 'lalala@#'
+
+  click_button 'Sign up'
+
 end
